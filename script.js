19785813a00cb30d06f45185f35a93cdc2c70566
@amritmohan <amritmohan2284@gmail.com>
2025-02-23 01:00:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+
+document.getElementById("enterBtn").addEventListener("click", function(){
+	let paragraphStatus = document.getEelementById("status");
+	paragrapghStatus.innerHTML = "<h1>Entered Metaverse</h1>";												 
+});
